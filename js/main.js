b7fecdfafa9c3bf3471a43b2798f18d46c659b21
Saintles <juls.kovalenko@gmail.com>
2026-02-15 17:58:03 +0600

    chore: add fontawesome.min to style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     var sidenav = document.querySelectorAll('.sidenav');
     var instances_sidenav = M.Sidenav.init(sidenav);
-  });
+    
+    var burger = document.querySelector('.burger');
+    
+    if (burger) {
+        burger.addEventListener('click', function(e) {
+            var menu = document.getElementById('slide-out');
+            var instance = M.Sidenav.getInstance(menu);
+            
+            if (instance) {
+                // Если меню открыто - закрываем его
+                if (instance.isOpen) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    instance.close();
+                }
+            }
+        });
+    }
+});
